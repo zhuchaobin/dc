66eede65a06d32ec7000a432afee1d223de238de
zhuchaobin <nanjing2007@163.com>
2018-11-01 21:19:02 +0800

    del

--- dc-biz/src/main/java/com/tianan/kltsp/dc/biz/mapper/T2ArAtchMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tianan.kltsp.dc.biz.mapper;
-
-import com.tianan.kltsp.dc.client.entity.T2ArAtch;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface T2ArAtchMapper extends Mapper<T2ArAtch> {
-}
--- dc-biz/src/main/java/com/tianan/kltsp/dc/biz/mapper/UserProtocolMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tianan.kltsp.dc.biz.mapper;
-
-
-import com.tianan.kltsp.dc.biz.core.Mapper;
-import com.tianan.kltsp.dc.client.entity.UserProtocol;
-
-public interface UserProtocolMapper extends Mapper<UserProtocol> {
-}
