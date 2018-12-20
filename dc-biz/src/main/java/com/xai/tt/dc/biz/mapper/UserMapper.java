1d9af3e24e9d96deaf7e00e67d79113e5a165e05
zhuchaobin <nanjing2007@163.com>
2018-12-20 21:31:27 +0800

    zhuchaobin

--- dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/UserMapper.java
+++ dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.xai.tt.dc.biz.mapper;
 
 import com.xai.tt.dc.client.model.User;
-import tk.mybatis.mapper.common.Mapper;
+import com.xai.tt.dc.biz.core.Mapper;
 
 public interface UserMapper extends Mapper<User> {
 }
