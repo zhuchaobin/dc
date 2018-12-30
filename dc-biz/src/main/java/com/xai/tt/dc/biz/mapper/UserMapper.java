2af3a1a0aea56a384c7c55d4716f00f173251fe8
zhuchaobin <nanjing2007@163.com>
2018-12-30 09:24:31 +0800

    555

--- dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/UserMapper.java
+++ dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package com.xai.tt.dc.biz.mapper;
 
 import com.xai.tt.dc.client.model.User;
+
+import java.util.List;
+
 import com.xai.tt.dc.biz.core.Mapper;
 
 public interface UserMapper extends Mapper<User> {
-	String QueryUserRoleParms(String username);
+	List<String> QueryUserRoleParms(String username);
 }
