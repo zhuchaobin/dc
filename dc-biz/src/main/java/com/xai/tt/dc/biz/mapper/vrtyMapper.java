a6614298ba88abe9925a94f09b9bae48ad4f395e
zhuchaobin <nanjing2007@163.com>
2019-03-08 00:37:48 +0800

    1

--- dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/vrtyMapper.java
+++ dc-biz/src/main/java/com/xai/tt/dc/biz/mapper/vrtyMapper.java
@@ -1,7 +1,7 @@
 package com.xai.tt.dc.biz.mapper;
 
 import com.xai.tt.dc.client.model.vrty;
-import tk.mybatis.mapper.common.Mapper;
+import com.xai.tt.dc.biz.core.Mapper;
 
 public interface vrtyMapper extends Mapper<vrty> {
 }
