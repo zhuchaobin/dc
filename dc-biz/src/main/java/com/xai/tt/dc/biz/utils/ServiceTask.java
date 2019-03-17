04f63f3d0ecc1c2c83ca5483bc2c43e13f182912
zhuchaobin <nanjing2007@163.com>
2019-03-18 00:18:03 +0800

    增加自动质押自动节点

--- dc-biz/src/main/java/com/xai/tt/dc/biz/utils/ServiceTask.java
+++ dc-biz/src/main/java/com/xai/tt/dc/biz/utils/ServiceTask.java
@@ -52,11 +52,11 @@ public void execute(DelegateExecution execution) {
 
 System.out.println("serviceTask已经执行已经执行！");
 
-String value1 = (String) text1.getValue(execution);
+/*String value1 = (String) text1.getValue(execution);
 
 System.out.println(value1);
 
-    execution.setVariable("var1", new StringBuffer(value1).reverse().toString());
+    execution.setVariable("var1", new StringBuffer(value1).reverse().toString());*/
 
 }
 
